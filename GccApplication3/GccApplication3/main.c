06d5ad7985563be0565f8fe843f66a939d03f37d
atharva-9 <53110598+atharva-9@users.noreply.github.com>
2020-04-09 16:16:57 +0530

    Add files via upload

---
+++ GccApplication3/GccApplication3/main.c
@@ -0,0 +1,285 @@
+/*
+ * GccApplication3.c
+ *
+ * Created: 12-03-2020 8.33.03 AM
+ * Author : Dark Warrior
+ */ 
+//PJ2 PJ3 PJ5 Radar sensor 
+//PJ1 PJ2 gps
+//XBEE 
+
+
+#define F_CPU 14745600
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+#include "motion.h"
+
+#define RS 0
+#define RW 1
+#define EN 2
+#define lcd_port PORTC
+unsigned char data; //to store received data from UDR1
+#define sbit(reg,bit)	reg |= (1<<bit)			// Macro defined for Setting a bit of any register.
+#define cbit(reg,bit)	reg &= ~(1<<bit)		// Macro defined for Clearing a bit of any register.
+
+void init_ports();
+void lcd_reset();
+void lcd_init();
+void lcd_wr_command(unsigned char);
+void lcd_wr_char(char);
+void lcd_line1();
+void lcd_line2();
+void lcd_string(char*);
+//Function to configure LCD port
+void lcd_port_config (void)
+{
+	DDRC = DDRC | 0xF7; //all the LCD pin's direction set as output
+	PORTC = PORTC & 0x80; // all the LCD pins are set to logic 0 except PORTC 7
+}
+void lcd_wr_command(unsigned char cmd)
+{
+	unsigned char temp;
+	temp = cmd;
+	temp = temp & 0xF0;
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+	
+	cmd = cmd & 0x0F;
+	cmd = cmd<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= cmd;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+//Function to Write Data on LCD
+void lcd_wr_char(char letter)
+{
+	char temp;
+	temp = letter;
+	temp = (temp & 0xF0);
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+
+	letter = letter & 0x0F;
+	letter = letter<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= letter;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+//Function to Print String on LCD
+void lcd_string(char *str)
+{
+	while(*str != '\0')
+	{
+		lcd_wr_char(*str);
+		str++;
+	}
+}
+
+
+//Function to Initialize LCD
+void lcd_init()
+{
+	_delay_ms(1);
+
+	lcd_wr_command(0x28);			//LCD 4-bit mode and 2 lines.
+	lcd_wr_command(0x01);
+	lcd_wr_command(0x06);
+	lcd_wr_command(0x0E);
+	lcd_wr_command(0x80);
+	
+}
+
+//Function to initialize ports
+void port_init()
+{
+	DDRB = 0x40;
+	lcd_port_config();
+PORTJ=0x00;
+DDRA = DDRA | 0x0F; //set direction of the PORTA 3 to PORTA 0 pins as output
+PORTA = PORTA & 0xF0; // set initial value of the PORTA 3 to PORTA 0 pins to logic 0
+DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM
+}
+
+
+//Function To Initialize UART0
+// desired baud rate:9600
+// actual baud rate:9600 (error 0.0%)
+// char size: 8 bit
+// parity: Disabled
+void uart0_init(void)
+{
+	UCSR0B = 0x00; //disable while setting baud rate
+	UCSR0A = 0x00;
+	UCSR0C = 0x06;
+	// UBRR0L = 0x47; //11059200 Hz
+	UBRR0L = 0x5F; // 14745600 Hzset baud rate lo
+	UBRR0H = 0x00; //set baud rate hi
+	UCSR0B = 0x98;
+}
+
+
+ISR(_VECTOR(26))        // ISR for receive complete interrupt
+{
+	data = UDR0;                //making copy of data from UDR0 in 'data'  variable
+
+	UDR0 = data;                //echo data back to PC
+
+	if(data == 0x37)
+	PORTB = 0x00;
+	if(data == 0x39)
+	PORTB = 0x40;
+
+}
+
+
+//Function To Initialize all The Devices
+void init_devices()
+{
+	cli(); //Clears the global interrupts
+	port_init();  //Initializes all the ports
+	lcd_init();
+	uart0_init(); //Initialize UART0 for serial communication
+	sei();   //Enables the global interrupts
+}
+
+void motions(void)
+{
+	
+forward(); //both wheels forward
+_delay_ms(40000);
+
+stop();
+_delay_ms(5000);
+left(); //Left wheel backward, Right wheel forward
+_delay_ms(12000);
+
+stop();
+_delay_ms(5000);
+forward(); //both wheels forward
+_delay_ms(5000);
+
+stop();
+_delay_ms(5000);
+left(); //Left wheel backward, Right wheel forward
+_delay_ms(12000);
+
+stop();
+_delay_ms(5000);
+forward(); //both wheels forward
+_delay_ms(40000);
+
+stop();
+_delay_ms(5000);
+stop();
+right(); //Left wheel backward, Right wheel forward
+_delay_ms(9000);
+forward(); //both wheels forward
+_delay_ms(5000);
+
+stop();
+_delay_ms(5000);
+
+stop();
+_delay_ms(5000);
+right(); //Left wheel backward, Right wheel forward
+_delay_ms(12000);
+
+}
+
+//Main Function
+int main(void)
+{
+	
+	init_devices();
+	//lcd_string(data);
+	lcd_string("GPS DATA:");
+	lcd_wr_command(0xc0);
+	lcd_string("LAT:");
+	_delay_ms(1000);
+	while(1)
+	{
+		
+	
+	while (PINJ0 == 0)
+	{
+			
+			forward(); //both wheels forward
+			_delay_ms(40000);
+
+			stop();
+			_delay_ms(5000);
+			left(); //Left wheel backward, Right wheel forward
+			_delay_ms(12000);
+
+			stop();
+			_delay_ms(5000);
+			forward(); //both wheels forward
+			_delay_ms(5000);
+
+			stop();
+			_delay_ms(5000);
+			left(); //Left wheel backward, Right wheel forward
+			_delay_ms(12000);
+
+			stop();
+			_delay_ms(5000);
+			forward(); //both wheels forward
+			_delay_ms(40000);
+
+			stop();
+			_delay_ms(5000);
+			stop();
+			right(); //Left wheel backward, Right wheel forward
+			_delay_ms(9000);
+			forward(); //both wheels forward
+			_delay_ms(5000);
+
+			stop();
+			_delay_ms(5000);
+
+			stop();
+			_delay_ms(5000);
+			right(); //Left wheel backward, Right wheel forward
+			_delay_ms(12000);
+
+	}
+	
+	
+	if (PINJ0 == 1) 
+	{
+		lcd_string("NO threat");
+		_delay_ms(100);
+		lcd_wr_command(0x01);
+		
+		
+	}
+	else
+	{
+		lcd_string("Threat  detected");
+		_delay_ms(100);
+		lcd_wr_command(0x01);
+	}
+	}
+	}
---
+++ GccApplication3/GccApplication3/motion.h
@@ -0,0 +1,77 @@
+/*
+ * motion.c
+ *
+ * Created: 26-02-2020 9.37.51 AM
+ * Author : Dark Warrior
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+
+
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+	unsigned char PortARestore = 0;
+
+	Direction &= 0x0F; 			// removing upper nibbel as it is not needed
+	PortARestore = PORTA; 			// reading the PORTA's original status
+	PortARestore &= 0xF0; 			// setting lower direction nibbel to 0
+	PortARestore |= Direction; 	// adding lower nibbel for direction command and restoring the PORTA status
+	PORTA = PortARestore; 			// setting the command to the port
+}
+
+
+void forward (void) //both wheels forward
+{
+	motion_set(0x06);
+}
+
+void back (void) //both wheels backward
+{
+	motion_set(0x09);
+}
+
+void left (void) //Left wheel backward, Right wheel forward
+{
+	motion_set(0x05);
+}
+
+void right (void) //Left wheel forward, Right wheel backward
+{
+	motion_set(0x0A);
+}
+
+void soft_left (void) //Left wheel stationary, Right wheel forward
+{
+	motion_set(0x04);
+}
+
+void soft_right (void) //Left wheel forward, Right wheel is stationary
+{
+	motion_set(0x02);
+}
+
+void soft_left_2 (void) //Left wheel backward, right wheel stationary
+{
+	motion_set(0x01);
+}
+
+void soft_right_2 (void) //Left wheel stationary, Right wheel backward
+{
+	motion_set(0x08);
+}
+
+void stop (void) //hard stop
+{
+	motion_set(0x00);
+}
+
+
+
+
+
