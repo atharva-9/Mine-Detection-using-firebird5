a4f2d016f5fbe78a878336c61d1406aa84aaafb8
atharva-9 <53110598+atharva-9@users.noreply.github.com>
2020-04-28 13:34:04 +0530

    Add files via upload

--- radar_testx.ino
+++ radar_testx.ino
@@ -1,32 +1,124 @@
 #include <LiquidCrystal.h>
 
 
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);// initialize the library with the numbers of the interface pins
 
-//#include"serial.h"
+// incoming serial byte
+int inByte = 0;         
+int east [ 15 ];
+int north [ 15 ];
+int i;
+int buzzer = PL1;
+void setup() 
+{
+pinMode(PJ2,INPUT);
+pinMode(PJ3,INPUT);
+pinMode(PJ5,INPUT);
 
-void setup() {
 
-pinMode(PH6,INPUT);
-pinMode(3,INPUT);
-pinMode(PE2,INPUT);
+  lcd.begin(16, 2);
+  lcd.print("Mine Detection Bot");
+  lcd.setCursor(0, 1);
+  lcd.print("Initialize ");
+}
+
+void gpstx()
+{
+
+    Serial.begin(4800);
+    delay(100);   
+//==================== searching for "GG" ===================//
+    do
+    {
+       do
+       {
+            while ( !Serial.available() );    
+        } while ( 'G' != Serial.read() );
+        while(!Serial.available());
+
+    } while ( 'G' != Serial.read() );
+
+//==================== searching for "GG" ===================//
+
+
+//============== seeking for north cordinate ==============//
+
+    do
+    {
+        while ( !Serial.available() );    
+    } while ( ',' != Serial.read() );
+    do
+    {
+        while ( !Serial.available() );    
+    } while ( ',' != Serial.read() );
+
+//============== seeking for north cordinate ==============//
+
+
+//============== printing the north cordinate ===============//
+
+//    Serial.print(" N: ");
+    i = 0;
+    do
+    {
+        while ( !Serial.available() ); 
+        inByte = Serial.read();
+        north [ i ] = inByte;
+//        Serial.write ( inByte );
+        i ++;
+    } while ( ',' != inByte );
+//============== printing the north cordinate ===============//
+
+//============== seeking for east cordinate ==============//
+    do
+    {
+        while ( !Serial.available() );    
+    } while ( ',' != Serial.read() );
+//============== seeking for east cordinate ==============//
+//============== printing the east cordinate ===============//
+//    Serial.print(" E: ");
+    i = 0;
+   do
+    {
+        while ( !Serial.available() ); 
+        inByte = Serial.read();
+        east [ i ] = inByte;
+       i ++;
+//        Serial.write ( inByte );
+    } while ( ',' != inByte );
+//============== printing the east cordinate ===============//
+    delay(500);
+    digitalWrite(buzzer, HIGH);       
+    Serial.begin(9600);
+    delay(100);
+    Serial.write(" GEOGRAPHICAL CORDINATES");
+    Serial.print( " N: " );
+    for ( i = 0; north [ i ] != ' '; i ++ )
+      Serial.write ( north [ i ] );
+    Serial.print( " E: " );    
+    for ( i = 0; east [ i ] != ' '; i ++ )
+      Serial.write ( east [ i ] );
+    Serial.println();
+    delay ( 500 );
+    digitalWrite(buzzer, LOW);  
+    exit;
 }
 void loop()
 {
-    int X;
+    int r1,r2,r3;
  Serial.begin(4800);
-  Serial.print("0"); 
-  //x=digitalRead(3);
- // Serial.print(x=analogRead(PH6));
-  //Serial.print(digitalRead(3));
-  Serial.print(X=analogRead(3));
-  
-  Serial.println(",2500");
-  delay(10);
-  if(X==168)
-  digitalWrite (13,HIGH);
-  //digitalWrite (3,HIGH);
-  
-  
-    
-  
+ 
+  Serial.print(r1=digitalRead(PJ2));
+  Serial.print(r2=digitalRead(PJ3));
+  Serial.print(r3=digitalRead(PJ5));
+ 
+  if((r1>0 && r2>0)||(r1>0 && r3>0)||(r2>0 && r3>0))
+  {
+    lcd.print("Threat detected");
+    gpstx();
+    while(buzzer,LOW)
+    {
+      
+    }
+    }
 }
